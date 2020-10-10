a52c244296e11e32407814195e48b264951ea487
Milan Zagorac <milan@pop-os.localdomain>
2020-10-10 20:21:00 +0200

    Positioning cards on the table

--- src/components/Cardboard.jsx
+++ src/components/Cardboard.jsx
@@ -7,9 +7,16 @@ import { flipCard, fetchIcon } from '../utils'
 
 const Cardboard = (props) => {
   const [icon, setIcon] = useState(null);
-  const { current: canvasWidth } = useRef(window.innerWidth * 0.65)
+  const { current: canvasWidth } = useRef(window.innerWidth * 0.65);
+  const canvasHeight = canvasWidth * 0.35;
+
   const cardWidth = canvasWidth * 0.15;
   const cardHeight = cardWidth * 1.35
+
+  const faceDownPosition = { x: 50, y: 50 };
+  const faceUpCardPosition = { x: 50 + cardWidth + 50, y: 50 };
+  const lastCardPosition = { x: canvasWidth - cardWidth - 50, y: 50 };
+  const iconPosition = { x: (2 * 50 + 2 * cardWidth + lastCardPosition.x) / 2, y: 50 }
 
   useEffect(() => {
     const res = fetchIcon(props.previousCard.value, props.currentCard.value);
@@ -17,23 +24,23 @@ const Cardboard = (props) => {
   }, [props.previousCard, props.currentCard]);
 
   return (
-    <Stage width={canvasWidth} height={canvasWidth * 0.35}>
+    <Stage width={canvasWidth} height={canvasHeight}>
       <Layer>
         <Img
           imgUrl={!props.cardRemaining ? '' : flipCard}
-          position={{ x: 50, y: 50 }}
+          position={ faceDownPosition }
           dim={{ width: cardWidth, height: cardHeight }}
         />
         <Img
           imgUrl={props.currentCard.image}
-          position={{ x: 350, y: 50 }}
+          position={faceUpCardPosition}
           dim={{ width: cardWidth, height: cardHeight }}
         />
-        <Img imgUrl={icon} position={{ x: 600, y: 100 }} dim={{ width: 55, height: 55 }} />
+        <Img imgUrl={icon} position={iconPosition} dim={{ width: 55, height: 55 }} />
         <Img
           imgUrl={props.previousCard.image}
-          position={{ x: 750, y: 50 }}
-          dim={{ width: 113, height: 157 }}
+          position={ lastCardPosition }
+          dim={{ width: cardWidth, height: cardHeight }}
         />
       </Layer>
     </Stage>
